6f31fef8ffc493e3eebf7aac985ada04a81e8932
Alati11 <peppealati@gmail.com>
2023-11-17 23:25:46 +0100

    add app-header-menu add props

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,38 @@
 <script>
 
+import AppHeaderMenu from './AppHeaderMenu.vue'
+
 
 export default {
-   
+    components: {
+        AppHeaderMenu,
+    },
+
     data() {
         return {
-            
-            
+            menu: [
+                {
+                    text: 'Home',
+                    href: '#',
+                },
+                {
+                    text: 'About',
+                    href: '#',
+                },
+                {
+                    text: 'Academics',
+                    href: '#',
+                },
+                {
+                    text: 'Courses',
+                    href: '#',
+                },
+                {
+                    text: 'News',
+                    href: '#',
+                },
+            ]
+
         }
     }
 }
@@ -14,20 +40,14 @@ export default {
 </script>
 
 <template>
-   <div class="header-container">
-    <div class="header-img">
-        <img class="header-logo" src="public/img/Logo.jpg" alt="">
+    <div class="header-container">
+        <div class="header-img">
+            <img class="header-logo" src="public/img/Logo.jpg" alt="">
+        </div>
+        <div class="header-link">
+            <AppHeaderMenu :menu="menu"/>
+        </div>
     </div>
-    <div class="header-link">
-        <ul class="header-link-menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Academics</a></li>
-            <li><a href="#">Courses</a></li>
-            <li><a href="#">News</a></li>
-        </ul>
-    </div>
-   </div>
 </template>
 
 <style lang="scss" scoped>
@@ -44,20 +64,13 @@ export default {
 
 .header-logo {
     height: 120px;
-    margin-top: 50px;
+    margin-top: 40px;
     z-index: 50%;
 }
+
 .header-link {
     margin-right: 250px;
     font-weight: 600;
-}
-.header-link-menu {
-    display: flex;
-    gap: 30px;
-}
-
-a:hover {
-    color: #FF3A2D;
 }
 
 </style>
---
+++ src/components/AppHeaderMenu.vue
@@ -0,0 +1,43 @@
+<script>
+
+
+export default {
+    props: ['menu'],
+        
+
+    data() {
+        return {
+        
+
+
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div>
+        <ul class="header-link-menu">
+            <li v-for="(link, i) in menu" :key="i">
+                <a :href="link.href">{{ link.text }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.header-link {
+    margin-right: 250px;
+    font-weight: 600;
+}
+
+.header-link-menu {
+    display: flex;
+    gap: 30px;
+}
+
+a:hover {
+    color: #FF3A2D;
+}
+</style>
